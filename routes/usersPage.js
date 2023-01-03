ed853035c27c949c1698faddd07035b2b5d53011
erikwehrmann <erik.wehrmann@gmail.com>
2023-01-03 22:30:00 +0000

    started users page

--- routes/usersPage.js
+++ routes/usersPage.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router  = express.Router();
 const db = require('../db/connection');
 
-router.get('/:id', (req, res) => {
-  const userId = req.session.userId;
-  if (!userId) {
+router.get('/', (req, res) => {
+  if (!req.session.userId) {
     res.status(401).send('Login to access page');
   } else {
-    // function to get user's own resources and liked resources
-    db.getMyResources(userId)
-    .then(myResources => res.send({myResources}))
+    db.query(`SELECT * FROM posts LEFT JOIN users ON owner_id = users.id WHERE owner_id = $1 GROUP BY posts.id, users.id`, [req.session.userId.id])
+    .then(result => {
+      console.log(result.rows);
+      // << create html elements for each result array item >> //
+      res.render('usersPage');
+    })
     .catch(e => {
       console.error(e);
       res.send(e)
