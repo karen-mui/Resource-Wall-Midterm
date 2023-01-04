6bb704f5f99d89032e0aa1ffbe4978f01fb61c2a
erikwehrmann <erik.wehrmann@gmail.com>
2023-01-04 00:03:44 +0000

    trying to figure out the users-page

--- routes/usersPage.js
+++ routes/usersPage.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const db = require('../db/connection');
+const { createPostElement } = require('../scripts/client');
 
 router.get('/', (req, res) => {
   if (!req.session.userId) {
@@ -8,13 +9,20 @@ router.get('/', (req, res) => {
   } else {
     db.query(`SELECT * FROM posts LEFT JOIN users ON owner_id = users.id WHERE owner_id = $1 GROUP BY posts.id, users.id`, [req.session.userId.id])
     .then(result => {
-      console.log(result.rows);
-      // << create html elements for each result array item >> //
-      res.render('usersPage');
+      result.rows.forEach(data => {
+        let post = createPostElement(data);
+        // let div = document.createElement('div');
+        // div.innerHTML = post;
+        // container.appendChild(div);
+        $('#users-posts').append(post);
+      })
     })
-    .catch(e => {
-      console.error(e);
-      res.send(e)
+    .then(
+      res.render('usersPage', { activeUser: req.session.userId })
+    )
+    .catch(err => {
+      console.error(err);
+      res.send(err)
     })
   }
 });
