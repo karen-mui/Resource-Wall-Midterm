f6791ba2ddcdbf10f898defd9417368e0a7f2ea8
Karen Mui <karen_mui@hotmail.com>
2023-01-06 08:09:00 +0000

    changed to dropdown menu, old search bar can be reinstated using the copy files

--- routes/search.js
+++ routes/search.js
@@ -6,7 +6,6 @@ router.get('/', (req, res) => {
   db.query(`
   SELECT * FROM topics WHERE topic LIKE '%' || $1 || '%'`, [req.query.searchResources])
     .then(result => {
-      console.log('req.params:', req.params);
       res.render('searchResults', { activeUser: req.session.userId, searchedTopic: result.rows[0]});
     })
     .catch(err => {
